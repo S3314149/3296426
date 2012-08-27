2d0af5bedd5505299f247c3de3d128b1c4b81648
Hongmin Gao <s3314149@yallara.cs.rmit.edu.au>
2012-08-27 11:18:10 +1000

    added

---
+++ html_formD.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC
+"-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html401/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Wines</title>
+</head>
+<body>
+<form action="query_results.php" method="GET">
+<?php
+  define('DB_HOST', 'yallara.cs.rmit.edu.au');
+  define('DB_PORT', '59552'); 
+  define('DB_NAME', 'winestore');
+  define('DB_USER', 'root'); 
+  define('DB_PW', 'gaohongmin'); 
+  try {
+$db = new PDO(
+"mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME,
+DB_USER,
+DB_PW
+);
+
+
+  function selectDistinct ($connection, 
+                           $tableName, 
+                           $attributeName, 
+                           $pulldownName, 
+                           $defaultValue) {
+       
+
+	   
+	   $defaultWithinResultSet = FALSE;
+	   $distinctQuery = "SELECT DISTINCT {$attributeName} FROM {$tableName}";
+       if (!($resultId = @ mysql_query ($distinctQuery, $connection))) showerror();
+
+        print "\n<select name=\"{$pulldownName}\">";
+
+         while ($row = @ mysql_fetch_array($resultId))
+          {$result = $row[$attributeName];
+           if (isset($defaultvalue) && $result == $defaultValue)
+                 print "\n\t<option selected value=\"{$result}\">{$result}";
+           else
+                 print "\n\t<option value=\"{$result}\">{$result}";
+        print "</option>";
+    }
+    print "\n</select>";
+  } 
+   if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {showerror();}
+   if (!mysql_select_db(DB_NAME, $connection)) {showerror(); }
+   print "\nWinery: ";
+   selectDistinct($connection, "winery", "winery_name", "wineryName", "All");
+?>
+<br>
+<input type="submit" name="submit" value="Show Wines">
+</form>
+</body>
+</html>
+
+
